578f461da850d339d32c151c2c02af8fa7a47cc8
Averek <singhavanish2003@gmail.com>
2021-11-13 16:24:34 +0530

    front commit

--- javi/src/components/Home.js
+++ javi/src/components/Home.js
@@ -1,24 +1,12 @@
-<<<<<<< HEAD
-import React from 'react'
-import Caro from './Caro'
-=======
 import React from "react";
 import Caro from "./Caro";
->>>>>>> c9714db8b2d57afe7c81b4483c79f8304ab035ac
 
 const Home = () => {
-  return <div>Hello World !</div>;
+  return (
+    <>
+      <Caro />
+    </>
+  );
 };
 
-<<<<<<< HEAD
-    return (
-        <div>
-        <Caro />
-        </div>
-    )
-}
-
-export default Home
-=======
 export default Home;
->>>>>>> c9714db8b2d57afe7c81b4483c79f8304ab035ac
--- javi/src/components/MyNavbar.js
+++ javi/src/components/MyNavbar.js
@@ -1,101 +1,5 @@
-<<<<<<< HEAD
 import React, { useEffect } from "react";
-import { Navbar, Nav, Container, Figure } from "react-bootstrap";
-import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
-import { auth, provider } from "../firebase";
-import {
-  selectUserName,
-  selectUserPhoto,
-  setUserLoginDetails,
-  setSignOutState,
-} from "../feature/userSch";
-
-const MyNavbar = () => {
-  const dispatch = useDispatch();
-  const history = useNavigate();
-  const userName = useSelector(selectUserName);
-  const userPhoto = useSelector(selectUserPhoto);
-  useEffect(() => {
-    auth.onAuthStateChanged(async (user) => {
-      if (user) {
-        setUser(user);
-        history("/home");
-      }
-    });
-    // eslint-disable-next-line
-  }, [userName]);
-
-  const handleAuth = () => {
-    if (!userName) {
-      auth
-        .signInWithPopup(provider)
-        .then((result) => {
-          setUser(result.user);
-          history("/home");
-        })
-        .catch((error) => {
-          alert(error.message);
-        });
-    } else if (userName) {
-      auth
-        .signOut()
-        .then(() => {
-          dispatch(setSignOutState());
-          history("/");
-        })
-        .catch((err) => alert(err.message));
-    }
-  };
-  const setUser = (user) => {
-    dispatch(
-      setUserLoginDetails({
-        name: user.displayName,
-        email: user.email,
-        photo: user.photoURL,
-      })
-    );
-  };
-
-  return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Container>
-        <Navbar.Brand href="/home">FIT-HUB</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        {!userName ? (
-          <Nav.Link onClick={handleAuth} style={{ color: "white" }}>
-            LOGIN
-          </Nav.Link>
-        ) : (
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="/dashboard">Dashboard</Nav.Link>
-              <Nav.Link href="/chatroom">Chat Room</Nav.Link>
-              <Nav.Link href="/contact">Contact Us</Nav.Link>
-              <Nav.Link href="/aboutus">About Us</Nav.Link>
-            </Nav>
-            <Nav>
-              <Figure>
-                <Figure.Image
-                  width={25}
-                  height={25}
-                  alt={userName}
-                  src={userPhoto}
-                />
-                <Nav.Link onClick={handleAuth}>LogOut</Nav.Link>
-              </Figure>
-            </Nav>
-          </Navbar.Collapse>
-        )}
-      </Container>
-    </Navbar>
-  );
-};
-
-export default MyNavbar;
-=======
-import React, { useEffect } from "react";
-import { Navbar, Nav, Container, Figure } from "react-bootstrap";
+import { Navbar, Nav, Container, Figure, Badge } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { auth, provider } from "../firebase";
@@ -155,38 +59,36 @@ const MyNavbar = () => {
       <Container>
         <Navbar.Brand href="#home">FIT-HUB</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#dashboard">Dashboard</Nav.Link>
-            <Nav.Link href="#chatroom">Chat Room</Nav.Link>\
-            <Nav.Link href="#aboutus">About Us</Nav.Link>
-          </Nav>
-          <Nav>
-            {!userName ? (
-              <Nav.Link onClick={handleAuth}>Login</Nav.Link>
-            ) : (
+        {!userName ? (
+
+          <Nav.Link onClick={handleAuth} style={{ color: "white", backgroundColor: "grey", borderRadius: "20px"}}><i class="far fa-user" style={{paddingRight: "5px"}}></i>LOGIN</Nav.Link>
+        ) : (
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/dashboard">Dashboard</Nav.Link>
+              <Nav.Link href="/chatroom">Chat Room</Nav.Link>
+              <Nav.Link href="/contact">Contact Us</Nav.Link>
+              <Nav.Link href="/about">About Us</Nav.Link>
+            </Nav>
+            <Nav>
               <>
-                {/* <Figure> */}
                 <div className="user-box">
                   <Figure.Image
                     width={30}
                     height={30}
-                    alt={userName}
                     src={userPhoto}
                     className="profile-img"
                   />
-                  {/* </Figure> */}
-                  <Nav.Link className="navbar-usrname">{userName}</Nav.Link>
+                  <Badge bg="danger">{userName}</Badge>
                 </div>
                 <Nav.Link onClick={handleAuth}>LogOut</Nav.Link>
               </>
-            )}
-          </Nav>
-        </Navbar.Collapse>
+            </Nav>
+          </Navbar.Collapse>
+        )}
       </Container>
     </Navbar>
   );
 };
 
 export default MyNavbar;
->>>>>>> c9714db8b2d57afe7c81b4483c79f8304ab035ac
