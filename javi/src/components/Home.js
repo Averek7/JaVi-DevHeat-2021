689a089f70a8161204d7981ff0c16c6a5d77b4c0
coder21 <coderinside21@gmail.com>
2021-11-13 12:07:21 +0530

    profile section in navbar added

--- javi/src/components/Home.js
+++ javi/src/components/Home.js
@@ -1,13 +1,8 @@
-import React from 'react'
+import React from "react";
+import Caro from "./Caro";
 
 const Home = () => {
-   
+  return <div>Hello World !</div>;
+};
 
-    return (
-        <div>
-            Hello World !
-        </div>
-    )
-}
-
-export default Home
+export default Home;
