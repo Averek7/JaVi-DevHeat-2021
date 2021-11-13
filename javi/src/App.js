f6cae05d154d0d6f1615ee61847cccaa0c4a853c
Averek <singhavanish2003@gmail.com>
2021-11-13 10:10:18 +0530

    auth

--- javi/src/App.js
+++ javi/src/App.js
@@ -1,9 +1,9 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
-// import Home from "./components/Home";
+import Home from "./components/Home";
 import MyNavbar from "./components/MyNavbar.js";
 import Caro from "./components/Caro.js";
-import { Alert } from "react-bootstrap";
+// import { Alert } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
@@ -11,9 +11,9 @@ function App() {
     <Router>
       <MyNavbar />
       <Caro />
-      {/* <Routes>
+      <Routes>
         <Route exact path="\home" element={<Home />} />
-      </Routes> */}
+      </Routes>
     </Router>
   );
 }
