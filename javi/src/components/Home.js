c043379c7b214d5bc09a1f2287f2e615a2dcf0c3
Averek <singhavanish2003@gmail.com>
2021-11-13 15:35:22 +0530

    Revert "commit"
    This reverts commit eb0884efa4e7ea22c315b2d3aded492640402b64.

--- javi/src/components/Home.js
+++ javi/src/components/Home.js
@@ -1,12 +1,24 @@
+<<<<<<< HEAD
 import React from 'react'
 import Caro from './Caro'
+=======
+import React from "react";
+import Caro from "./Caro";
+>>>>>>> c9714db8b2d57afe7c81b4483c79f8304ab035ac
 
 const Home = () => {
+  return <div>Hello World !</div>;
+};
 
+<<<<<<< HEAD
     return (
         <div>
         <Caro />
         </div>
     )
 }
+
+export default Home
+=======
 export default Home;
+>>>>>>> c9714db8b2d57afe7c81b4483c79f8304ab035ac
