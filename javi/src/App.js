eb0884efa4e7ea22c315b2d3aded492640402b64
Averek <singhavanish2003@gmail.com>
2021-11-13 12:25:18 +0530

    commit

--- javi/src/App.js
+++ javi/src/App.js
@@ -1,32 +1,18 @@
-<<<<<<< HEAD
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css';
-import Home from './components/Home';
-import MyNavbar from './components/MyNavbar';
-import Login from './components/Login';
-=======
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./components/Home";
 import MyNavbar from "./components/MyNavbar.js";
-import Caro from "./components/Caro.js";
-// import { Alert } from "react-bootstrap";
->>>>>>> c9714db8b2d57afe7c81b4483c79f8304ab035ac
+// import { Alert } from "react-bootstrap";c
 import "bootstrap/dist/css/bootstrap.min.css";
+import Login from "./components/Login";
 
 function App() {
   return (
     <Router>
       <MyNavbar />
-<<<<<<< HEAD
       <Routes>
-        <Route exact path='/' element={<Login />}/>
+        <Route exact path='/' element={<Login/>}/>
         <Route exact path='/home'element={<Home/>}/>
-=======
-      <Caro />
-      <Routes>
-        <Route exact path="\home" element={<Home />} />
->>>>>>> c9714db8b2d57afe7c81b4483c79f8304ab035ac
       </Routes>
     </Router>
   );
