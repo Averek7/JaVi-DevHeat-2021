53d0a73c336e2d3000431773b0fbfc1208129f7f
Averek <singhavanish2003@gmail.com>
2021-11-14 17:10:39 +0530

    chat

--- javi/src/components/ChatRoom.js
+++ javi/src/components/ChatRoom.js
@@ -1,11 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from "react";
+import db, { auth } from "../firebase";
+import SendMsg from "./SendMsg";
+import "../css/Chat.css";
 
 const ChatRoom = () => {
-    return (
-        <div>
-            Chat-Room
-        </div>
-    )
-}
+  const scroll = useRef();
+  const [messages, setMessages] = useState([]);
+  useEffect(() => {
+    db.collection("messages")
+      .orderBy("createdAt")
+      .limit(50)
+      .onSnapshot((snapshot) => {
+        setMessages(snapshot.docs.map((doc) => doc.data()));
+      });
+  }, []);
+  return (
+    <>
+      <div className="msgs">
+        {messages.map(({ id, text, photoURL, uid }) => (
+          <div>
+            <div
+              key={id}
+              className={`msg ${
+                uid === auth.currentUser.uid ? "sent" : "received"
+              }`}
+            >
+              <img src={photoURL} alt="" />
+              <p>{text}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+      <SendMsg scroll={scroll} />
+      <div ref={scroll}></div>
+    </>
+  );
+};
 
-export default ChatRoom
+export default ChatRoom;
